3e1435f39463e0f3e1a4af1b9641c5a08db417d8
Noor Jabeen <156057742+noori03@users.noreply.github.com>
2024-06-05 11:35:32 +0530

    Time: 17 ms (20.95%), Space: 19.6 MB (5.10%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<string> result;
+        for(char ch = 'a' ; ch<='z'; ch++){
+            int minCount = INT_MAX;
+            for(string word : words){
+                int count =0;
+                for(char c : word){
+                    if(c==ch)count++;
+                }
+                minCount= min(minCount,count);
+            }
+            for(int i=0;i<minCount;i++){
+                string s ;
+                s.push_back(ch);
+                result.push_back(s);
+            }
+        }
+        return result;
+    }
+};
