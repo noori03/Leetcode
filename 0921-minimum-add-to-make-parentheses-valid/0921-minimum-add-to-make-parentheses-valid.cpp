daa8ed534a5c629aeb121899d993fb8f659833b1
Noor Jabeen <156057742+noori03@users.noreply.github.com>
2024-10-09 13:50:16 +0530

    Time: 4 ms (18.73%), Space: 7.8 MB (81.46%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack <char> st;
+        for(auto it:s){
+            if(st.empty()) st.push(it);
+            else if(st.top()=='(' && it==')') st.pop();
+            else st.push(it);
+        }
+        return st.size();
+    }
+};
