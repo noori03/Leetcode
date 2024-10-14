59175c2ca6404733ccee8d133132fd7f63276aea
Noor Jabeen <156057742+noori03@users.noreply.github.com>
2024-10-14 12:37:07 +0530

    Time: 192 ms (66.58%), Space: 79.7 MB (64.84%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.cpp
@@ -3,13 +3,13 @@ public:
     long long maxKelements(vector<int>& nums, int k) {
         priority_queue<int> pq;
         for(auto it: nums)pq.push(it);
+        
         long long ans=0;
         while(k--){
-             ans += pq.top();
-             int a = pq.top();
-             pq.pop();
-             if(a%3==0) pq.push(a/3);
-             else pq.push(a/3 + 1);
+            int a = pq.top();
+            pq.pop();
+            ans += a;
+            pq.push((a+2)/3);
         }
         return ans;
     }
